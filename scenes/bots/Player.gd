ed9b3c4f1b20653b2a81f40955aa284020b1873b
Vic Ben <crackbbbb@gmail.com>
2020-04-03 00:23:43 +0800

    before major changes weapons

--- scenes/bots/Player.gd
+++ scenes/bots/Player.gd
@@ -3,9 +3,9 @@ extends "res://scenes/bots/_BaseBot.gd"
 
 onready var bar_weapon_heat: = $Bars/WeaponHeat
 onready var bar_charge_level: = $Bars/ChargeLevel
-#onready var bar_shoot_cooldown = $Bars/ShootCooldown
 onready var hud_weapon_slots: = $PlayerUI/WeaponSlots
 onready var ui_inventory: = $PlayerUI/Inventory
+#onready var bar_shoot_cooldown = $Bars/ShootCooldown
 
 const PLAYER_BARS_OFFSET: int = 15
 const HEAT_BAR_WARNING_THRESHOLD: float = 0.75
@@ -13,19 +13,23 @@ const SLOT_LABEL_COLOR: = Color(0.13, 0.27, 0.15) #low value green
 const WEAP_OVERHEAT_COLOR: = Color(0.9, 0, 0) #red
 const WEAP_HEAT_COLOR = Color(1, 0.7, 0.15) #orange
 
-var dict_weapon_hud: Dictionary = {}
+var dict_weapons: Dictionary = {}
 var current_slot_selected: int = -1
 var is_inventory_open: bool = false
 
 
 func _ready() -> void:
 	#player specific stats
-	_set_up_player_hud()
+	_set_up_player_ui()
 	_set_up_player_default_vars()
 	update_player_vars()
 
 
-func _set_up_player_hud() -> void:
+func _set_up_player_ui() -> void:
+	update_weapon_hud()
+
+
+func update_weapon_hud() -> void:
 	var selected_slot_initialized: = false
 	var i: = -1
 	for slot in $Weapons.get_children():
@@ -38,15 +42,15 @@ func _set_up_player_hud() -> void:
 			selected_slot_initialized = true
 		#initialize hud
 		var weap = slot.get_node("Weapon")
+		dict_weapons[weap] = hud_weapon_slots.get_node("Slot" + i as String)
 		hud_weapon_slots.get_node("Slot" + i as String + "/WeaponSprite").texture = weap.get_node("Sprite").texture
 		hud_weapon_slots.get_node("Slot" + i as String + "/SlotHeat").max_value = weap.heat_capacity
-		dict_weapon_hud[weap] = hud_weapon_slots.get_node("Slot" + i as String)
 
 
 func _set_up_player_default_vars() -> void:
 	bar_weapon_heat.rect_position.x -= bar_weapon_heat.rect_size.y + bot_radius + PLAYER_BARS_OFFSET
-#	bar_weapon_heat.rect_position.x = bar_shoot_cooldown.rect_position.x - PLAYER_BARS_OFFSET
 	bar_charge_level.rect_position.x += bot_radius + PLAYER_BARS_OFFSET
+#	bar_weapon_heat.rect_position.x = bar_shoot_cooldown.rect_position.x - PLAYER_BARS_OFFSET
 
 
 func update_player_vars() -> void:
@@ -115,7 +119,7 @@ func _update_hud_elements() -> void:
 		if slot.has_node("Weapon") == false:
 			continue
 		var weap = slot.get_node("Weapon")
-		var hud_weap_heat = dict_weapon_hud[weap].get_node("SlotHeat")
+		var hud_weap_heat = dict_weapons[weap].get_node("SlotHeat")
 		if weap.is_overheating == true:
 			hud_weap_heat.modulate = WEAP_OVERHEAT_COLOR
 		else:
