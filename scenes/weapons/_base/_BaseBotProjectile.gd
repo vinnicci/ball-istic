fe7bb8f7638abe14298e5a97559b187f0083043d
Vic Ben <crackbbbb@gmail.com>
2020-05-15 19:38:17 +0800

    small changes

--- scenes/weapons/_base/_BaseBotProjectile.gd
+++ scenes/weapons/_base/_BaseBotProjectile.gd
@@ -4,6 +4,7 @@ extends "res://scenes/bots/_base/_BaseBotUnarmed.gd"
 func init_travel(proj_position: Vector2, proj_direction: float, origin: bool) -> void:
 	global_position = proj_position
 	apply_central_impulse(Vector2(2000, 0).rotated(proj_direction))
+	hostile = origin
 	$Timers/Lifetime.start()
 
 
