fc7fe18d94cfefc52311f0be8fbb491879ac8573
Vic Ben <crackbbbb@gmail.com>
2020-10-14 18:49:38 +0800

    ai update

--- scenes/bots/_ais/kiter ai/KiterAI.gd
+++ scenes/bots/_ais/kiter ai/KiterAI.gd
@@ -3,10 +3,25 @@ extends "res://scenes/bots/_ais/_base/_BaseAI.gd"
 
 export (int) var max_shooting_dist: int = 500
 export (int) var enemy_too_close_dist: int = 250
-export (int) var max_flee_dist: int = 700
+export (int) var max_flee_dist: int = 1000
 
 
 func _ready() -> void:
 	_params_dict["max_shooting"] = max_shooting_dist
 	_params_dict["enemy_too_close"] = enemy_too_close_dist
-	_params_dict["max_flee"] = max_flee_dist
+
+
+var _current_enemy_proj: PackedScene
+
+
+func _process(delta: float) -> void:
+	if _check_if_valid_bot(_enemy) == true && _enemy.current_weapon != null:
+		var enemy_proj: PackedScene = _enemy.current_weapon.Projectile
+		if _current_enemy_proj == enemy_proj:
+			return
+		_current_enemy_proj = enemy_proj
+		if enemy_proj == null:
+			_params_dict["max_flee"] = max_flee_dist
+		else:
+			var sample_proj = enemy_proj.instance()
+			_params_dict["max_flee"] = sample_proj.proj_range + 300
